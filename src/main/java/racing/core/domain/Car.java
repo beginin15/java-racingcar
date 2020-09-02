b5d4901bad85254d824e508077bc98c46eefd400
Jay <beginin15@gmail.com>
2020-09-02 16:41:21 +0900

    refactor: 생성자 중복 코드 제거

--- src/main/java/racing/core/domain/Car.java
+++ src/main/java/racing/core/domain/Car.java
@@ -13,9 +13,7 @@ public final class Car implements Comparable<Car> {
     private final int position;
 
     public Car(String name) {
-        validateName(name);
-        this.name = name;
-        this.position = 0;
+        this(name, 0);
     }
 
     public Car(String name, int position) {
